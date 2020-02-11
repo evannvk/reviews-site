a28fc2d25a2cefd6e12a3a2d736d6b202c219be9
Evan Novak <evannvk@gmail.com>
2020-02-10 20:59:55 -0500

    Finished project

--- src/main/java/org/wecancodeit/ReviewRepository.java
+++ src/main/java/org/wecancodeit/ReviewRepository.java
@@ -10,8 +10,18 @@ import org.springframework.stereotype.Repository;
 public class ReviewRepository {
 
 	private Map<Long, Review> reviewList = new HashMap<>();
-	private Review reviewOne = new Review(1, "Review One", "category", "image url", "content");
-	private Review reviewTwo = new Review(2, "review two", "category", "image url", "content");
+	private Review reviewOne = new Review(1, "Eternal Sunshine of the Spotless Mind", "Movies", "/images/Eternal-Sunshine.jpg", 
+			"Eternal sunshine of the spottless mind is one of my favortie movies. It takes the classic American Romance movies and turns it on its head by " +
+			"showing the fragility of memory and love. After Joel Barrish (Jim Carrey) and Clementine Kruczynski (Kate Winslet) find each other on a trip " +
+			"skipping work, a relationship starts to bud after a few nights spent together. Soon we discover this is not a new relationship, and the two had " +
+			"broken up recently and Clementine had her memory erased, eager to move on with her life. Upon discovering this, Joel decides to have his memory erased " +
+			"as well. But as the procedure is being performed, the memories of fondness and tender love give him second thoughts and begins to fight the operation." +
+			"We then follow Joel in his desperate quest to prevent the procedure.");
+	private Review reviewTwo = new Review(2, "Maus", "Comics", "/images/maus.jpg", 
+			"Maus, a two-part graphic novel written and drawn by Art Spiegelman, depicts his father's tale as a Jewish man during the holocaust. The graphic novel " +
+			"depitcs the horrifying historical event with cats and mice. Yes, the Jewish people are depicted as mice and the nazis as cats. As interesting as the " +
+			"holocaust is, the story is just as much about that as it is the realationship with Art and his father, Vladek. It reads as a memoir of Art's interviews" +
+			"with his father twoards the end of his life.");
 
 	public ReviewRepository() {
 		reviewList.put(reviewOne.getId(), reviewOne);
